1e020be2d714bd5f59fa3e34098fd25b738779ce
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-14 14:07:39 -0400

    finishing header picture

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <div className="h-screen">  
+    <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import desktopBG from '../images/bg-pattern-intro-desktop.svg';
+
+
+export default function Header(){
+    return(
+        <div className=" flex header-div h-3/4 relative">
+           
+            
+        </div>
+    )
+}
+
