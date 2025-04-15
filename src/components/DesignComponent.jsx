489bf4847de61fcb93b0eeeff5ae089b2df3febe
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-14 22:14:29 -0400

    really done for the night

--- src/components/DesignComponent.jsx
+++ src/components/DesignComponent.jsx
@@ -2,11 +2,11 @@ import editorLogo from "../images/illustration-editor-desktop.svg";
 
 export default function DesignComponent(){
     return (
-        <div className=" min-h-screen  flex flex-col ">
-             <h2 className="text-center  z-10 mt-16">test33</h2>
-            <div className="design-div  relative   flex-col md:flex-row items-center md:items-start flex ">
+        <div className=" min-h-screen relative flex flex-col mb-64">
+             <h2 className="text-center text-darkBlue font-semibold absolute top-24 w-full text-4xl z-10 ">Designed for the future</h2>
+            <div className="design-div  relative flex-col md:flex-row items-center md:items-start flex ">
                
-                <div className=" px-32 flex-1 flex flex-col  mt-58  text-base/8">
+                <div className=" px-32 flex-1 flex flex-col  mt-58 gap-16 text-base/8">
                    <div>
                         <p className="text-2xl mb-8 text-darkBlue font-semibold">Introducing an extensible editor</p>
                         <p className="text-graish font-extralight">Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
